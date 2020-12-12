13c9875682a40cbf26f21d843958612cdbe8e0f5
sathyaramkumar <70505050+sathyaramkumar@users.noreply.github.com>
2020-12-12 21:34:11 +0530

    style to title bar

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// title bar
+function notificationDrop() {
+    var notificationDropDown = document.getElementById("dropdown");
+    if (notificationDropDown.style.display === "block") {
+        notificationDropDown.style.display = "none";
+    }else {
+        notificationDropDown.style.display = "block";
+    }
+}
+
+function profileDrop() {
+    var profileDropDown = document.getElementById("profileDropDown");
+    if (profileDropDown.style.display === "block") {
+        profileDropDown.style.display = "none";
+    }else {
+        profileDropDown.style.display = "block";
+    }
+}
